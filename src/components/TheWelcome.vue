76a6c98f841d5f9cb0da2e8491a1d6cde4a5cfd2
Johannes Copeland <contact@johannescopeland.com>
2023-08-29 22:11:54 +0200

    Removed last component of list

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -72,15 +72,4 @@ import SupportIcon from './icons/IconSupport.vue'
     <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
     twitter account for latest news in the Vue world.
   </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
 </template>
