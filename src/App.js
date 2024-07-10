b4d12b76872184835a949b0eedb33abb98dc2114
Johannes Copeland <contact@johannescopeland.com>
2024-07-10 19:56:43 +0200

    Added first socials

--- src/App.js
+++ src/App.js
@@ -6,18 +6,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Johannes Copeland</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm a Software Engineer working with and studying Data and AI.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <h1>Connect with me</h1>
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/johannscopeland" target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="https://www.xing.com/profile/yourxingurl" target="_blank" rel="noopener noreferrer">
+              XING
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
